9a7a1f942eb1cb50bebe38cdb29b16dc624f9d39
Magnus <mazejumper01@gmail.com>
2025-06-13 13:28:12 +0200

    changed a bit of the footer and header also corected the CardHome button

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
 import Footer from "./components/Footer";
+import Header from './components/Header';
 import "./App.css";
 
 function Layout() {
   return (
     <>
-      <header></header>
+      <Header />
 
       <main>
         <Outlet />
