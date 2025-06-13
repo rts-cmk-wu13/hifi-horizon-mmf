e482f32bb986b939b1c014726532379415b8b40c
Your Name <you@example.com>
2025-06-13 13:52:24 +0200

    loder og contact og about us

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,9 +1,10 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
