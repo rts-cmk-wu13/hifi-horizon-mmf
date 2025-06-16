b1352c844596f7485e2540c7b81ed2517fcf2a91
mohamed Osman <smartbot616@gmail.com>
2025-06-16 08:58:45 +0200

    updated loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import router from "./Router.jsx";
 
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
